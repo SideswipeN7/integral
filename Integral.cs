e9f2afd38fc8b9fab9188671e91e491032c6fb40
Paweł Wanot <pawel.wanot@gmail.com>
2019-03-09 14:38:56 +0100

    Initial commit

---
+++ Integral.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MonteCarlo
+{
+    internal class Integral
+    {
+        private Random random_ = new Random(DateTime.Now.Millisecond);
+        public double StartX { get; set; }
+        public double EndX { get; set; }
+        public double EndY { get; set; }
+        public double StartY { get; set; }
+        public int Accuracy { get; set; }
+        private double value_;
+        public double Value { get => value_; }
+        public Func<double, double> func { get; set; }
+        private DateTime startTime_;
+        private DateTime endTime_;
+
+        private double RandomPoint() => StartX + random_.NextDouble() * (EndX - StartX);
+
+        private int funcIn(double x, double y)
+        {
+            if ((y > 0) && (y <= func(x)))
+                return 1;
+            else if ((y > 0) && (y <= func(x)))
+                return -1;
+            return 0;
+        }
+
+        public void Calculate()
+        {
+            int pointsIn = 0;
+            startTime_ = DateTime.Now;
+            for (int i = 0; i < Accuracy; i++)
+            {
+                pointsIn += mainCalcFunc();
+            }
+            value_ = CalculateValue(pointsIn);
+            endTime_ = DateTime.Now;
+        }
+
+        public void CalculateParalel()
+        {
+            int pointsIn = 0;
+            Task[] taskArray = new Task[Accuracy];
+            Object _lock = new Object();
+            startTime_ = DateTime.Now;
+            for (int i = 0; i < taskArray.Length; i++)
+            {
+                taskArray[i] = Task.Factory.StartNew((Object obj) =>
+                {
+                    int value = mainCalcFunc();
+                    lock (obj)
+                    {
+                        pointsIn += value;
+                    }
+                }, _lock);
+            }
+            Task.WaitAll(taskArray);
+            value_ = CalculateValue(pointsIn);
+            endTime_ = DateTime.Now;
+        }
+
+        private int mainCalcFunc() => funcIn(RandomPoint(), RandomPoint());
+
+        private double CalculateValue(double pointsIn) => (pointsIn / (Accuracy * 1.0)) * ((EndX - StartX) * (EndY - StartY));
+
+        public string CalculationTime() => $"{(endTime_ - startTime_)}";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MonteCarlo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int accuracy = 5000;
+            Integral integral = new Integral()
+            {
+                StartX = -10,
+                EndX = 10,
+                StartY = -10,
+                EndY = 10,
+                Accuracy = accuracy,
+                func = (x) => (Math.Pow(x,12) + Math.Pow(x, -4/5) * x + 3)
+            };
+
+            Console.WriteLine("Początek obliczeń");
+            Console.WriteLine("==================================================");
+
+            Console.WriteLine("Liczenie całki");
+            Console.WriteLine("Jeden wątek");
+            integral.Calculate();
+            Console.WriteLine($"Wartość: {integral.Value}");
+            Console.WriteLine($"Czas obliczeń: {integral.CalculationTime()}");
+
+            Console.WriteLine("==================================================");
+
+            Console.WriteLine($"{accuracy} wątków");
+            integral.CalculateParalel();
+            Console.WriteLine($"Wartość: {integral.Value}");
+            Console.WriteLine($"Czas obliczeń: {integral.CalculationTime()}");
+
+            Console.WriteLine("==================================================");
+            Console.WriteLine("Koniec obliczeń");
+            Console.ReadKey();
+        }
+    }
+}
